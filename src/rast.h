b8a46f19c06f0e919eafb4b3e4a42b3d4da8081c
Elias Vassilakis <vaselas99@gmail.com>
2021-07-10 06:44:25 +0300

    3d rendering

--- src/rast.h
+++ src/rast.h
@@ -33,7 +33,7 @@ internal void rend_clear(RenderingContext *rc, vec4 clear_color)
 		rc->data[4 * i + 2] = clear_color.elements[2];
 		rc->data[4 * i + 3] = clear_color.elements[3];
 		//clear fbo depth info
-		rc->zbuf[i] = -10000.f;
+		rc->zbuf[i] = 1000.f;
 	}
 
 }
@@ -74,7 +74,7 @@ internal void rend_line(RenderingContext *rc, ivec2 t0, ivec2 t1, vec4 color)
 	}
 }
 
-vec3 barycentric(ivec2 *points, ivec3 P)
+vec3 barycentric(vec3 *points, ivec3 P)
 {
     vec3 u = vec3_cross(v3(points[2].x - points[0].x, points[1].x - points[0].x, points[0].x - P.x),v3(points[2].y - points[0].y, points[1].y - points[0].y, points[0].y - P.y));
     if (fabs(u.z) < 1.f)
@@ -82,7 +82,7 @@ vec3 barycentric(ivec2 *points, ivec3 P)
     return v3(1.f - (u.x +u.y)/u.z, u.y/u.z, u.x/u.z);
 }
 
-void triangle(ivec2 *points, vec4 color)
+void triangle(vec3 *points, vec4 color)
 { 
     ivec2 bboxmin = iv2(rc.render_width-1, rc.render_height-1);
     ivec2 bboxmax = iv2(0,0);
@@ -112,7 +112,7 @@ void triangle(ivec2 *points, vec4 color)
                 P.z += points[i].elements[2]*bc_screen.elements[i];
             u32 index = P.x + P.y * rc.render_width;
             //if our pixel is closer than the zbuf's current, we render
-            if (rc.zbuf[index] < P.z)
+            if (rc.zbuf[index] > P.z)
             //if (TRUE)
             {
                 rc.zbuf[index] = P.z;
@@ -138,10 +138,61 @@ internal void init(void)
 	rc.blend_func = FALSE; //no blending!
 
     //look_at(vec3 eye, vec3 center, vec3 fake_up)
-    view = look_at(v3(0,0,-50), v3(0,0,0), v3(0,1,0));
+    view = look_at(v3(0,0,-10), v3(0,0,0), v3(0,1,0));
     proj = perspective_proj(45.f,rc.render_width/ (f32)rc.render_height, 0.1f,100.f); 
 
+    /*
     sphere_data = gen_sphere_data(1.f, 8, 8);
+    for (u32 i = 0; i < sphere_verts_count; ++i)
+    {
+        vec4 local_coords = v4(sphere_data[i].pos.x, sphere_data[i].pos.y, sphere_data[i].pos.z, 1.f);
+        mat4 t = mat4_translate(v3(0,0,0));
+        t.elements[0][0] = 1.f;
+        t.elements[1][1] = 1.f;
+        t.elements[2][2] = 1.f;
+        vec4 ndc_coords = mat4_mulv(mat4_mul(proj, mat4_mul(view,t)), local_coords);
+        //perspective divide??
+        ndc_coords.x /= ndc_coords.z;
+        ndc_coords.y /= ndc_coords.z;
+        ivec2 screen_coords = iv2(((ndc_coords.x + 1.f)/2) * rc.render_width, ((ndc_coords.y + 1.f)/2.f) * rc.render_height); 
+        sphere_data[i].pos = (vec3){screen_coords.x, screen_coords.y, ndc_coords.z};
+    }
+    */
+
+   /* 
+    for (u32 i = 0; i < 36; ++i)
+    {
+        vec4 local_coords = v4(cube_data[i].pos.x, cube_data[i].pos.y, cube_data[i].pos.z, 1.f);
+        mat4 t = mat4_mul(mat4_translate(v3(0,0,0)), mat4_rotate(28.f, v3(0,1,0.2)));
+        t.elements[0][0] = 1.f;
+        t.elements[1][1] = 1.f;
+        t.elements[2][2] = 1.f;
+        vec4 ndc_coords = mat4_mulv(mat4_mul(proj, mat4_mul(view,t)), local_coords);
+        //perspective divide??
+        ndc_coords.x /= ndc_coords.z;
+        ndc_coords.y /= ndc_coords.z;
+        ivec2 screen_coords = iv2(((ndc_coords.x + 1.f)/2) * rc.render_width, ((ndc_coords.y + 1.f)/2.f) * rc.render_height); 
+        cube_data[i].pos = (vec3){screen_coords.x, screen_coords.y, ndc_coords.z};
+    }
+    */
+
+}
+internal vec3 project_point(vec3 coords)
+{
+    vec4 local_coords = v4(coords.x, coords.y, coords.z, 1.f);
+    mat4 t = mat4_mul(mat4_translate(v3(0,0,0)), mat4_rotate(p.current_time * 40, v3(0,1,0)));
+    t.elements[0][0] = 1.f;
+    t.elements[1][1] = 1.f;
+    t.elements[2][2] = 1.f;
+    vec4 ndc_coords = mat4_mulv(mat4_mul(proj, mat4_mul(view,t)), local_coords);
+    //perspective divide??
+    ndc_coords.x /= ndc_coords.w;
+    ndc_coords.y /= ndc_coords.w;
+    ndc_coords.z /= ndc_coords.w;
+    ivec2 screen_coords = iv2(((ndc_coords.x + 1.f)/2) * rc.render_width, ((ndc_coords.y + 1.f)/2.f) * rc.render_height); 
+    vec4 wc = mat4_mulv(t, local_coords);
+    return (vec3){screen_coords.x, screen_coords.y, ndc_coords.z};
+
 }
 
 internal void update(f32 dt)
@@ -155,7 +206,7 @@ internal void update(f32 dt)
 
 internal void render(void)
 {
-	rend_clear(&rc, v4(0,0,0,1));
+	rend_clear(&rc, v4(0.05,0.05,0.05,1));
 
     ivec2 origin = (ivec2){rc.render_width / 2, rc.render_height / 2};
     ivec2 dest = (ivec2){origin.x + cos(p.current_time)*rc.render_width/4.f, origin.y + sin(p.current_time)*rc.render_width /4.f};
@@ -169,20 +220,27 @@ internal void render(void)
         ivec2 v2 = (ivec2){(quad_verts[i+2].pos.x + 1.f) * 100, (quad_verts[i+2].pos.y + 1.f) * 100};
         rend_line(&rc, v0, v1, v4(1,0,0,1));
         rend_line(&rc, v1, v2, v4(0,1,0,1));
-        rend_line(&rc, v2, v0, v4(0,0,1,1));
+        rend_line(&rc, v2,- v0, v4(0,0,1,1));
     }
     */
 
-    for (u32 i = 0; i < sphere_verts_count; i+=3)
+    //here we transform all points from local coordinate -> global coords -> view coords -> NDC coords -> screen coords
+    for (u32 i = 0; i < 36; i+=3)
     {
-        ivec2 points[3] = { (ivec2){(sphere_data[i].pos.x + 2.f) * 100, (sphere_data[i].pos.y + 2.f) * 100},
-        (ivec2){(sphere_data[i+1].pos.x + 2.f) * 100, (sphere_data[i+1].pos.y + 2.f) * 100},
-        (ivec2){(sphere_data[i+2].pos.x + 2.f) * 100, (sphere_data[i+2].pos.y + 2.f) * 100}};
+        vec3 points[3] = { (vec3){cube_data[i].pos.x, cube_data[i].pos.y,cube_data[i].pos.z},
+                             (vec3){cube_data[i+1].pos.x, cube_data[i+1].pos.y,cube_data[i+1].pos.z},
+                             (vec3){cube_data[i+2].pos.x, cube_data[i+2].pos.y,cube_data[i+2].pos.z}};
+
+        points[0] = project_point(points[0]);
+        points[1] = project_point(points[1]);
+        points[2] = project_point(points[2]);
+
 
 
 
         vec4 base_color = v4(0.7,0.3,0.3,1.f);
-        f32 intensity = vec3_dot(vec3_normalize(sphere_data[i].norm), vec3_normalize(light_dir));
+        f32 intensity = vec3_dot(vec3_normalize(cube_data[i].norm), vec3_normalize(light_dir));
+        intensity = (i+2) / (f32)36;
         base_color = vec4_mulf(base_color, intensity);
 #if defined(EPILEPSY)
         if (intensity > 0)
@@ -192,6 +250,5 @@ internal void render(void)
             triangle(points, base_color);
 #endif
     }
-
 }
 #endif
